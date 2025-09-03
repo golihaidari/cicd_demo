1cd981e65f0fcbe409ac9062392427c049ae2207
golihaidari <golihaidari@gmail.com>
2025-09-03 14:05:44 +0200

    update code

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, CI/CD!");
-        //System.out.println("x is chosen by system.");
+        System.out.println("x is chosen by system.");
         int x = countdown(3);
         System.out.println("x: " + x);
     }
