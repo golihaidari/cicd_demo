0481a81dcce4c2832de46b68724c3af8eb18b033
golihaidari <golihaidari@gmail.com>
2025-09-03 12:40:09 +0200

    create project

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, CI/CD!");
+        //System.out.println("x is chosen by system.");
+        int x = countdown(3);
+        System.out.println("x: " + x);
+    }
+    
+    // helper method for testing
+    public static int countdown(int start) {
+        int x = start;
+        while (x != 0) {
+            x--;
+        }
+        return x;
+    }
+}
